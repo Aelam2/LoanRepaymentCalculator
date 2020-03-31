e00144ca2595e0e7b5de6603404c6839cebec64a
Alex Elam <aelam@grimco.com>
2020-03-31 11:27:16 -0500

    Dashboard INIT

--- frontend/src/pages/DashboardPage/index.js
+++ frontend/src/pages/DashboardPage/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
+import GridContent from "components/GridContent";
+import AnalysisRow from "./AnalysisRow";
+import PageLoading from "components/PageLoading";
 import styles from "./DashboardPage.module.scss";
+
+import { visitData } from "__tests__/mockData/charts";
 
 class DashboardPage extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <GridContent>
+        <Suspense fallback={<PageLoading />}>
+          <AnalysisRow loading={false} error={false} data={visitData} />
+        </Suspense>
+      </GridContent>
+    );
   }
 }
 
