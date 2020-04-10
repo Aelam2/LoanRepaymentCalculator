f9995096173cd6fd6d444a0471d339dfe5f653a3
Alex Elam <aelam@grimco.com>
2020-04-10 13:28:50 -0500

    GET/POST Payment Plans

--- backend/lib/middleware/route-validator/paymentPlanRules.js
+++ backend/lib/middleware/route-validator/paymentPlanRules.js
@@ -1,11 +1,19 @@
 import { body, param } from "express-validator";
 
 const getPaymentPlanRules = () => {
-  return [body("PlanName", "Plan name is required").isLength({ min: 1 })];
+  return [];
 };
 
 const createPaymentPlanRules = () => {
-  return [body("PlanName", "Plan name is required").isLength({ min: 1 })];
+  return [
+    body("PlanName", "Plan name is required").isLength({ min: 1 }),
+    body("AllocationMethodID", "Allocation method is required").isLength({ min: 1 }),
+    body("IsCurrent", "Plan activation must be specified").isInt({ min: 0, max: 1 }),
+    body("Payments", "Plan activation must be specified").isArray(),
+    body("Payments.*.PaymentDate", "Payment must have a valid date").isISO8601(),
+    body("Payments.*.PaymentAmount", "Payment Amount must be a number").isFloat({ min: 1 }),
+    body("Payments.*.RecurringTypeID", "Payment reccurement must be specified").isInt()
+  ];
 };
 
 const updatePaymentPlanRules = () => {
--- backend/lib/routes/PaymentPlans.js
+++ backend/lib/routes/PaymentPlans.js
@@ -1,5 +1,7 @@
 import express from "express";
 import validate, { getPaymentPlanRules, createPaymentPlanRules, updatePaymentPlanRules, deletePaymentPlanRules } from "../middleware/route-validator";
+import { PaymentPlans as PaymentPlansModel, Payments as PaymentsModel } from "../models/models";
+
 let router = express.Router();
 
 /**
@@ -15,13 +17,65 @@ router
   /**
    * @swagger
    *
-   * /payment-plans:
+   * /me/payment-plans:
    *  get:
    *    description: Get a list of all payment plans
    *    tags: [Payment Plans]
+   *    responses:
+   *      '200':
+   *        description: Retrieve all non-deleted Payment Plans created by a user
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                status:
+   *                  type: string
+   *                  example: success
+   *                data:
+   *                  type: array
+   *                  items:
+   *                    properties:
+   *                      PaymentPlanID:
+   *                        $ref: '#/components/schemas/PaymentPlans/properties/PaymentPlanID'
+   *                      UserID:
+   *                        $ref: '#/components/schemas/PaymentPlans/properties/UserID'
+   *                      PlanName:
+   *                        $ref: '#/components/schemas/PaymentPlans/properties/PlanName'
+   *                      AllocationMethodID:
+   *                        $ref: '#/components/schemas/PaymentPlans/properties/AllocationMethodID'
+   *                      IsCurrent:
+   *                        $ref: '#/components/schemas/PaymentPlans/properties/IsCurrent'
+   *                      DateCreated:
+   *                        $ref: '#/components/schemas/PaymentPlans/properties/DateCreated'
+   *                      DateUpdated:
+   *                        $ref: '#/components/schemas/PaymentPlans/properties/DateUpdated'
+   *                      DateDeleted:
+   *                        $ref: '#/components/schemas/PaymentPlans/properties/DateDeleted'
+   *                      Payments:
+   *                        type: array
+   *                        items:
+   *                          $ref: '#/components/schemas/Payments'
+   *      '500':
+   *         description: An unexpected error occured
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/FiveHundredError'
    */
   .get(getPaymentPlanRules(), validate, async (req, res) => {
     try {
+      let { UserID } = req.user;
+      let paymentPlans = await PaymentPlansModel.findAll({
+        where: { UserID },
+        include: [
+          {
+            model: PaymentsModel
+          }
+        ]
+      });
+
+      res.status(200).json({ status: "success", data: paymentPlans });
     } catch (err) {
       res.status(500).json({ status: "error", data: null, error: "an unexpected error occured" });
     }
@@ -30,14 +84,111 @@ router
   /**
    * @swagger
    *
-   * /payment-plans:
+   * /me/payment-plans:
    *  post:
    *    description: Create a new payment plan
    *    tags: [Payment Plans]
+   *    requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *            required:
+   *              - PlanName
+   *              - AllocationMethodID
+   *              - IsCurrent
+   *              - Payments
+   *            properties:
+   *              PlanName:
+   *                $ref: '#/components/schemas/PaymentPlans/properties/PlanName'
+   *              AllocationMethodID:
+   *                $ref: '#/components/schemas/PaymentPlans/properties/AllocationMethodID'
+   *              IsCurrent:
+   *                $ref: '#/components/schemas/PaymentPlans/properties/IsCurrent'
+   *              Payments:
+   *                type: array
+   *                items:
+   *                  type: object
+   *                  properties:
+   *                    PaymentDate:
+   *                      $ref: '#/components/schemas/Payments/properties/PaymentDate'
+   *                    PaymentAmount:
+   *                      $ref: '#/components/schemas/Payments/properties/PaymentAmount'
+   *                    RecurringTypeID:
+   *                      $ref: '#/components/schemas/Payments/properties/RecurringTypeID'
+   *
+   *    responses:
+   *      '200':
+   *        description: New payment plan and initial payments were successfully created
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                status:
+   *                  type: string
+   *                  example: success
+   *                data:
+   *                  type: object
+   *                  properties:
+   *                    PlanName:
+   *                      $ref: '#/components/schemas/PaymentPlans/properties/PlanName'
+   *                    AllocationMethodID:
+   *                      $ref: '#/components/schemas/PaymentPlans/properties/AllocationMethodID'
+   *                    IsCurrent:
+   *                      $ref: '#/components/schemas/PaymentPlans/properties/IsCurrent'
+   *                    Payments:
+   *                      type: array
+   *                      items:
+   *                        $ref: '#/components/schemas/Payments'
+   *      '422':
+   *        description: Invalid input for a field
+   *        content:
+   *          application/json:
+   *            schema:
+   *               $ref: '#/components/schemas/FourTwentyTwoError'
+   *      '500':
+   *         description: An unexpected error occured
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/FiveHundredError'
    */
   .post(createPaymentPlanRules(), validate, async (req, res) => {
     try {
+      let { UserID } = req.user;
+      let { PlanName, AllocationMethodID, IsCurrent, Payments } = req.body;
+
+      // Create new payment plan
+      let newPlan = await PaymentPlansModel.create({
+        UserID,
+        PlanName,
+        AllocationMethodID,
+        IsCurrent
+      });
+
+      // Loop through provided payments and create a promise to create payment
+      let paymentCreationPromises = [];
+      for (let payment of Payments) {
+        let { PaymentDate, PaymentAmount, RecurringTypeID } = payment;
+
+        paymentCreationPromises.push(
+          PaymentsModel.create({
+            PaymentPlanID: newPlan.PaymentPlanID,
+            PaymentDate,
+            PaymentAmount,
+            RecurringTypeID
+          })
+        );
+      }
+
+      // Create new individual payments associated to new plan
+      let newPayments = await Promise.all(paymentCreationPromises);
+
+      res.status(200).json({ status: "success", data: { ...newPlan.toJSON(), Payments: newPayments.map(p => p.toJSON()) } });
     } catch (err) {
+      console.log(err.message);
       res.status(500).json({ status: "error", data: null, error: "an unexpected error occured" });
     }
   });
